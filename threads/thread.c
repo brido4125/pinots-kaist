c9ef63fa47489773a27f9e766ea6fe8a62c8174e
brido4125 <hcs4125@gmail.com>
2022-11-23 04:44:58 +0000

    fix:test_max_priority and pass hex_dump

--- threads/thread.c
+++ threads/thread.c
@@ -403,9 +403,14 @@ thread_set_priority (int new_priority) {
 void test_max_priority(int new_priority){
 	if (list_empty(&ready_list))
 		return ; //  좀 더 좋은방법 있으면 알려주셈.
-	if ((list_entry(list_front(&ready_list), struct thread, elem)->priority  < new_priority))
-		return;
-	thread_yield();
+	int run_priority = thread_current()->priority;
+	struct list_elem *e= list_begin(&ready_list);
+	struct thread *t = list_entry(e, struct thread, elem);
+    
+	if (run_priority < t->priority)
+	{
+		thread_yield();
+	}
 }
 
 /* Returns the current thread's priority. */
