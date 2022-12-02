9f3620b60b43a31ea1bc9bfb2bf400c43bf9d4ea
brido4125 <hcs4125@gmail.com>
2022-12-02 07:29:28 +0000

    update:12/2 오후 4시 29분

--- include/vm/anon.h
+++ include/vm/anon.h
@@ -1,11 +1,18 @@
 #ifndef VM_ANON_H
 #define VM_ANON_H
 #include "vm/vm.h"
+#include "lib/kernel/bitmap.h"
 struct page;
 enum vm_type;
 
 struct anon_page {
+    struct page anon_p;
+    int swap_sector; // swap된 내용이 저장되는 sector
 };
+
+// [include>vm>anon.h] 추가
+struct bitmap *swap_table; // 0 - empty, 1 - filled
+int bitcnt;
 
 void vm_anon_init (void);
 bool anon_initializer (struct page *page, enum vm_type type, void *kva);
