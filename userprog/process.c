4e298d1b1276ef70edbadcb971821927f3bd2ad3
Ubuntu <ubuntu@ip-172-31-13-127.ap-northeast-2.compute.internal>
2022-12-04 07:07:33 +0000

    22-12-04 16:07

--- userprog/process.c
+++ userprog/process.c
@@ -797,7 +797,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 		/* Project3 - Anon Page */
 		struct container* container = (struct container*)malloc(sizeof(struct container));
 		container->file = file;
-		container->page_read_bytes = page_read_bytes;
+		container->read_bytes = page_read_bytes;
         container->offset = ofs;
 
 		if (!vm_alloc_page_with_initializer (VM_ANON, upage,
