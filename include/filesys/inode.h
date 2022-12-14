cd5170a74f1e1de5344cd712ec6b934f79dc32b1
brido4125 <hcs4125@gmail.com>
2022-12-14 07:52:42 +0000

    fix:헤더파일로 구조체 이동

--- include/filesys/inode.h
+++ include/filesys/inode.h
@@ -20,4 +20,24 @@ void inode_deny_write (struct inode *);
 void inode_allow_write (struct inode *);
 off_t inode_length (const struct inode *);
 
+/* On-disk inode.
+ * Must be exactly DISK_SECTOR_SIZE bytes long. */
+struct inode_disk {
+	disk_sector_t start;                /* First data sector. */
+	off_t length;                       /* File size in bytes. */
+	unsigned magic;                     /* Magic number. */
+    uint32_t is_dir;                    /* Dir or File */
+    
+};
+
+/* In-memory inode. */
+struct inode {
+	struct list_elem elem;              /* Element in inode list. */
+	disk_sector_t sector;               /* Sector number of disk location. */
+	int open_cnt;                       /* Number of openers. */
+	bool removed;                       /* True if deleted, false otherwise. */
+	int deny_write_cnt;                 /* 0: writes ok, >0: deny writes. */
+	struct inode_disk data;             /* Inode content. */
+};
+
 #endif /* filesys/inode.h */
